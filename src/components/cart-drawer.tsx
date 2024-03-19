c2282904eacc4177381b6af01086954fbd5a5f7f
toumani <toumani49@gmail.com>
2024-03-19 13:03:24 +0000

    Update cart items quantity

--- src/components/cart-drawer.tsx
+++ src/components/cart-drawer.tsx
@@ -1,18 +1,43 @@
+import { useMemo } from "react";
 import { useGlobalContext } from "../global-context.tsx";
-import { XMarkIcon } from "@heroicons/react/24/solid";
+import { XMarkIcon, CreditCardIcon } from "@heroicons/react/24/solid";
 import CartItemCard from "./cart-item-card.tsx";
+import { ShoppingCartIcon } from "@heroicons/react/24/solid";
+import { Button } from "@nextui-org/react";
 
 export default function CartDrawer() {
 	const { isDrawerOpen, setDrawerOpen, cartItems } = useGlobalContext()
 
+	const grandTotal = useMemo(() => {
+		let sum = 0
+		for (let i = 0; i < cartItems.length; i++)
+			sum += cartItems[i].quantity * cartItems[i].product.price
+
+		return sum
+	}, [cartItems])
+
 	return (
-		<section className={`fixed w-[90vw] z-50 top-0 bottom-0 right-0 p-4 ${isDrawerOpen ? "translate-x-0" : "translate-x-[100vw]"} transition-transform bg-white`}>
+		<section className={`flex flex-col fixed w-[90vw] z-50 top-0 bottom-0 right-0 p-4 ${isDrawerOpen ? "translate-x-0" : "translate-x-[100vw]"} transition-transform bg-white drop-shadow-lg bg-white`}>
 			<header className="flex justify-between">
-				<h2 className="font-bold text-2xl">Cart</h2>
+				<h2 className="flex gap-2 items-center font-bold text-2xl">
+					<ShoppingCartIcon className="h-6 w-6 text-black" />
+					Cart
+				</h2>
 				<XMarkIcon className="h-8 w-8 text-gray-700 cursor-pointer hover:text-gray-400" strokeWidth={2} onClick={() => setDrawerOpen(false)}/>
 			</header>
-			<div>
+			<div className="p-4 space-y-4 grow overflow-y-auto">
 				{ cartItems.map(item => <CartItemCard key={item.product.id} cartItem={item} />) }
+			</div>
+			<div className="text-right space-y-4 py-4 bg-gray-50">
+				<p className="text-xl font-bold">{ `Grand Total: $${ grandTotal.toFixed(2) }`}</p>
+				<Button
+					startContent={<CreditCardIcon className="h-6 w-6 text-white" />}
+					color="success"
+					className="text-white font-bold"
+					isDisabled={grandTotal === 0}
+				>
+					Checkout
+				</Button>
 			</div>
 		</section>
 	)
--- src/components/cart-item-card.tsx
+++ src/components/cart-item-card.tsx
@@ -1,11 +1,60 @@
+import { Button, Image } from "@nextui-org/react";
+import { TrashIcon } from "@heroicons/react/24/solid"
 import { CartItem } from "../lib/types.ts";
+import { useGlobalContext } from "../global-context.tsx";
+import { useCallback } from "react";
 
 interface CartItemProps {
 	cartItem: CartItem,
 }
 
-export default function CartItemCard({ cartItem }: CartItemProps) {
+export default function CartItemCard({ cartItem: { product, quantity } }: CartItemProps) {
+	const { setCartItems } = useGlobalContext()
+
+	const removeFromCart = useCallback(() => {
+		setCartItems(prevState => prevState.filter(item => item.product.id !== product.id))
+	}, [product.id, setCartItems])
+
+	const updateQuantity = useCallback((operation: "+" | "-") => {
+		setCartItems(prevState => {
+			const item = prevState.find(item => item.product.id === product.id)
+			if (item)
+				item.quantity = operation === "+" ? item.quantity + 1 : item.quantity - 1
+			return [...prevState]
+		})
+	}, [product.id, setCartItems])
+
+	const increaseQuantity = useCallback(() => updateQuantity("+"), [updateQuantity])
+
+	const decreaseQuantity = useCallback(() => updateQuantity("-"), [updateQuantity])
+
 	return (
-		<div>{ cartItem.product.title } - { cartItem.quantity }</div>
+		<div className="bg-white drop-shadow-lg rounded-lg">
+			<div className="flex items-center gap-4 p-1">
+				<Image
+					shadow="sm"
+					radius="lg"
+					width="100%"
+					alt={product.title}
+					className="w-12 object-cover"
+					src={product.image}
+				/>
+				<div className="grow">
+					<h4 className="font-bold">{ product.title }</h4>
+					<div className="flex items-center">
+						<Button isIconOnly className="min-w-8 w-8 p-0" size="sm" color="default" isDisabled={quantity === 1} onClick={decreaseQuantity}>-</Button>
+						<span className="w-8 text-center">{ quantity }</span>
+						<Button isIconOnly className="min-w-8 w-8 p-0" size="sm" color="default" onClick={increaseQuantity}>+</Button>
+					</div>
+				</div>
+				<Button isIconOnly color="danger" variant="ghost" className="self-start" onClick={removeFromCart}>
+					<TrashIcon className="h-4 w-4 text-danger" />
+				</Button>
+			</div>
+			<div className="text-right p-2">
+				<p className="text-base font-light">{`Unit price: $${ product.price }`}</p>
+				<p className="text-lgm font-semibold">{`Total: $${ (product.price * quantity).toFixed(2) }`}</p>
+			</div>
+		</div>
 	)
 }
